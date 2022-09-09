73fe75f5db98e041e934c0dbdc30b8e7688f53c9
Judit Bancells Vélez <judit@juditbancells.cat>
2022-09-09 17:02:29 +0200

    add switch statement

--- js-intro/index.js
+++ js-intro/index.js
@@ -1,11 +1,41 @@
-const minAgeDrinking = parseInt(
-  prompt("Let me know your age to check if you can legally drink alcohol")
+// const minAgeDrinking = parseInt(
+//   prompt("Let me know your age to check if you can legally drink alcohol")
+// );
+
+// if (minAgeDrinking < 18) {
+//   console.log("No puedes beber alcohol!");
+// } else if (minAgeDrinking >= 70) {
+//   console.log("Puedes beber alcohol pero no te pases!");
+// } else {
+//   console.log("Puede beber sin problemas.");
+// }
+
+const askMembersWuTang = prompt(
+  "Let me know you favourite member of the Wu Tang Clan and I'll tell you theit neighborhood"
 );
 
-if (minAgeDrinking < 18) {
-  console.log("No puedes beber alcohol!");
-} else if (minAgeDrinking >= 70) {
-  console.log("Puedes beber alcohol pero no te pases!");
-} else {
-  console.log("Puede beber sin problemas.");
+let nghoodWuTangMember = "";
+
+switch (askMembersWuTang) {
+  case "RZA":
+  case "Raekwon":
+  case "GZA":
+  case "U-God":
+  case "Masta Killa":
+    nghoodWuTangMember = "Brooklyn";
+    break;
+  case "Ol' Dirty Bastard":
+    nghoodWuTangMember = "Fort Greene";
+    break;
+  case "Method Man":
+  case "Ghostface Killah":
+  case "Inspectah Deck":
+  case "Cappadonna":
+    nghoodWuTangMember = "Staten Island";
+    break;
+  default:
+    nghoodWuTangMember = "New York New York";
+    break;
 }
+
+console.log(`${askMembersWuTang} is from ${nghoodWuTangMember}`);
